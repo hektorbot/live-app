186516e578409861d2e597ad140e844c9ae735c6
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-07-05 22:53:50 -0400

    chore: Deployments setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-param-reassign': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
---
+++ shipitfile.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const consola = require('consola');
+const program = require('commander');
+const path = require('path');
+
+const errors = [];
+const { DEPLOY_PATH, DEPLOY_HOST } = process.env;
+if (!DEPLOY_HOST) {
+  errors.push(new Error('DEPLOY_HOST must be defined'));
+}
+if (!DEPLOY_PATH) {
+  errors.push(new Error('DEPLOY_PATH must be defined'));
+}
+if (errors.length) {
+  errors.forEach(consola.error);
+  process.exit();
+}
+
+program.option('-r, --revision <rev>', 'Branch, commit or tag', 'master').parse(process.argv);
+
+module.exports = (shipit) => {
+  // eslint-disable-next-line global-require
+  require('shipit-deploy')(shipit);
+  shipit.initConfig({
+    default: {
+      branch: program.revision,
+      repositoryUrl: 'https://github.com/hektorbot/live-app.git',
+      servers: DEPLOY_HOST,
+    },
+    prod: {
+      deployTo: path.join(DEPLOY_PATH, 'production'),
+    },
+  });
+
+  shipit.on('updated', () => {
+    shipit.remote(`
+      cd ${shipit.releasePath};
+      ${['.env'].map(file => `ln -s ${path.join(shipit.config.deployTo, file)}`).join(';')}
+      sudo systemctl restart nginx;
+      `);
+  });
+};
